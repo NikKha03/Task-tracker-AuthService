3ffc61b22dd688e1923b0e56917630360cd857ed
NikKha03 <kolyakhalimendik@gmail.com>
2024-08-15 15:13:43 +0300

    начало командной работы

--- src/main/java/sharpBubbles/authService/controllers/TaskController.java
+++ src/main/java/sharpBubbles/authService/controllers/TaskController.java
@@ -22,91 +22,132 @@ public class TaskController {
     @Value("${services.task}")
     private String taskService;
 
-    @GetMapping("/all")
-    public ResponseEntity<?> getAllTasks(Principal principal) {
-        Long currentUserId = userService.getPrincipalUserId(principal);
-        String response = restTemplate.getForObject(taskService + '/' + currentUserId + "/allTasks", String.class);
-
-        return ResponseEntity.ok(response);
-    }
-
     @GetMapping("/allCompleted")
-    public ResponseEntity<?> getAllCompleted(Principal principal) {
-        Long currentUserId = userService.getPrincipalUserId(principal);
-        String response = restTemplate.getForObject(taskService + '/' + currentUserId + "/allCompletedTasks", String.class);
+    public ResponseEntity<?> getAllCompleted(Principal principal,
+                                             @RequestParam(required = false) String ownerType,
+                                             @RequestParam(required = false) Long teamId) {
+
+        Long ownerId = null;
+        if (ownerType.equals("USER")) {
+            ownerId = userService.getPrincipalUserId(principal);
+        }
+        if (ownerType.equals("TEAM")) {
+            ownerId = teamId;
+        }
+        String response = restTemplate.getForObject(taskService + '/' + ownerId + "/allCompletedTasks" + getParams(ownerType, teamId), String.class);
 
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/onTheDay")
-    public ResponseEntity<?> getTasksOnTheDay(Principal principal) {
-        Long currentUserId = userService.getPrincipalUserId(principal);
-        String response = restTemplate.getForObject(taskService + '/' + currentUserId + "/tasksOnTheDay", String.class);
+    public ResponseEntity<?> getTasksOnTheDay(Principal principal,
+                                              @RequestParam(required = false) String ownerType,
+                                              @RequestParam(required = false) Long teamId) {
+
+        Long ownerId = null;
+        if (ownerType.equals("USER")) {
+            ownerId = userService.getPrincipalUserId(principal);
+        }
+        if (ownerType.equals("TEAM")) {
+            ownerId = teamId;
+        }
+        String response = restTemplate.getForObject(taskService + '/' + ownerId + "/tasksOnTheDay" + getParams(ownerType, teamId), String.class);
 
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/onOtherDays")
-    public ResponseEntity<?> getTasksOnOtherDays(Principal principal) {
-        Long currentUserId = userService.getPrincipalUserId(principal);
-        String response = restTemplate.getForObject(taskService + '/' + currentUserId + "/tasksOnOtherDays", String.class);
+    public ResponseEntity<?> getTasksOnOtherDays(Principal principal,
+                                                 @RequestParam(required = false) String ownerType,
+                                                 @RequestParam(required = false) Long teamId) {
+
+        Long ownerId = null;
+        if (ownerType.equals("USER")) {
+            ownerId = userService.getPrincipalUserId(principal);
+        }
+        if (ownerType.equals("TEAM")) {
+            ownerId = teamId;
+        }
+        String response = restTemplate.getForObject(taskService + '/' + ownerId + "/tasksOnOtherDays" + getParams(ownerType, teamId), String.class);
 
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/incomplete")
-    public ResponseEntity<?> getTasksIncomplete(Principal principal) {
-        Long currentUserId = userService.getPrincipalUserId(principal);
-        String response = restTemplate.getForObject(taskService + '/' + currentUserId + "/tasksIncomplete", String.class);
+    public ResponseEntity<?> getTasksIncomplete(Principal principal,
+                                                @RequestParam(required = false) String ownerType,
+                                                @RequestParam(required = false) Long teamId) {
+
+        Long ownerId = null;
+        if (ownerType.equals("USER")) {
+            ownerId = userService.getPrincipalUserId(principal);
+        }
+        if (ownerType.equals("TEAM")) {
+            ownerId = teamId;
+        }
+        String response = restTemplate.getForObject(taskService + '/' + ownerId + "/tasksIncomplete" + getParams(ownerType, teamId), String.class);
 
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/onSomeday")
-    public ResponseEntity<?> getAllInProgressWithoutDatePlannedImplementation(Principal principal) {
-        Long currentUserId = userService.getPrincipalUserId(principal);
-        String response = restTemplate.getForObject(taskService + '/' + currentUserId + "/allInProgressTasksWithoutDatePlannedImplementation", String.class);
+    public ResponseEntity<?> getAllInProgressWithoutDatePlannedImplementation(Principal principal,
+                                                                              @RequestParam(required = false) String ownerType,
+                                                                              @RequestParam(required = false) Long teamId) {
+
+        Long ownerId = null;
+        if (ownerType.equals("USER")) {
+            ownerId = userService.getPrincipalUserId(principal);
+        }
+        if (ownerType.equals("TEAM")) {
+            ownerId = teamId;
+        }
+        String response = restTemplate.getForObject(taskService + '/' + ownerId + "/allInProgressTasksWithoutDatePlannedImplementation" + getParams(ownerType, teamId), String.class);
 
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/create")
     public ResponseEntity<?> createTask(Principal principal, @RequestBody Object request) {
-        Long currentUserId = userService.getPrincipalUserId(principal);
-        String response = restTemplate.postForObject(taskService + '/' + currentUserId + "/createTask", request, String.class);
+        Long ownerId = userService.getPrincipalUserId(principal);
+        String response = restTemplate.postForObject(taskService + '/' + ownerId + "/createTask", request, String.class);
 
         return ResponseEntity.ok(response);
     }
 
     @PutMapping("/change/{taskId}")
     public ResponseEntity<?> changeTask(Principal principal, @PathVariable Long taskId, @RequestBody Object request) {
-        Long currentUserId = userService.getPrincipalUserId(principal);
-        restTemplate.put(taskService + '/' + currentUserId + "/changeTask/" + taskId, request);
+        Long ownerId = userService.getPrincipalUserId(principal);
+        restTemplate.put(taskService + '/' + ownerId + "/changeTask/" + taskId, request);
 
         return ResponseEntity.ok("Task successfully modified!");
     }
 
     @DeleteMapping("/delete/{taskId}")
     public ResponseEntity<?> deleteTask(Principal principal, @PathVariable Long taskId) {
-        Long currentUserId = userService.getPrincipalUserId(principal);
-        restTemplate.delete(taskService + '/' + currentUserId + "/deleteTask/" + taskId);
+        Long ownerId = userService.getPrincipalUserId(principal);
+        restTemplate.delete(taskService + '/' + ownerId + "/deleteTask/" + taskId);
 
         return ResponseEntity.ok("Task successfully deleted!");
     }
 
     @PutMapping("/changeStatusOnCompleted/{taskId}")
     public ResponseEntity<?> changeTaskStatusOnCompleted(Principal principal, @PathVariable Long taskId, @RequestBody Object request) {
-        Long currentUserId = userService.getPrincipalUserId(principal);
-        restTemplate.put(taskService + '/' + currentUserId + "/changeTaskStatusOnCompleted/" + taskId, request);
+        Long ownerId = userService.getPrincipalUserId(principal);
+        restTemplate.put(taskService + '/' + ownerId + "/changeTaskStatusOnCompleted/" + taskId, request);
 
         return ResponseEntity.ok("Task status has been successfully changed to completed!");
     }
 
     @PutMapping("/changeStatusOnInProgress/{taskId}")
     public ResponseEntity<?> changeTaskStatusOnInProgress(Principal principal, @PathVariable Long taskId, @RequestBody Object request) {
-        Long currentUserId = userService.getPrincipalUserId(principal);
-        restTemplate.put(taskService + '/' + currentUserId + "/changeTaskStatusOnInProgress/" + taskId, request);
+        Long ownerId = userService.getPrincipalUserId(principal);
+        restTemplate.put(taskService + '/' + ownerId + "/changeTaskStatusOnInProgress/" + taskId, request);
 
         return ResponseEntity.ok("Task status has been successfully changed to completed!");
+    }
+
+    private String getParams(String ownerType, Long teamId) {
+        return String.format("?ownerType=%s", ownerType);
     }
 }
